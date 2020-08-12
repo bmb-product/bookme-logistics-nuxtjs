0c64cd6526513207d73f2514cb29d370ac9c5faf
Channa Ly <channa.info@gmail.com>
2020-08-12 10:44:17 +0700

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="center examplex">
+    <vs-navbar center-collapsed v-model="active">
+      <template #left>
+        <img src="~/assets/images/logo.jpg" height="32" />
+      </template>
+
+      <vs-navbar-item :active="active == 'guide'" id="guide">
+        Guide
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'docs'" id="docs">
+        Documents
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'components'" id="components">
+        Components
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'license'" id="license">
+        license
+      </vs-navbar-item>
+
+      <template #right>
+        <vs-button flat>Login</vs-button>
+        <vs-button>Get Started</vs-button>
+      </template>
+    </vs-navbar>
+
+    <div class="square">
+      <div class="child">
+        <Nuxt />
+      </div>
+
+      <div class="child">child2</div>
+      <div class="child">child3</div>
+    </div>
+  </div>
+</template>
+
+<style>
+html,
+body {
+  background: #eee;
+}
+</style>
